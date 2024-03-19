10fbe1d3b4eccb364d2b3f84b47e349d26106c98
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-03-19 11:38:26 -0500

    Add error spreadsheet generation; improve error handling

--- error.ts
+++ error.ts
@@ -1,4 +1,12 @@
-import { ImpressDataType } from "./schema";
+import {
+  Contact,
+  Customer,
+  ImpressDataType,
+  LineItem,
+  Order,
+  Product,
+} from "./schema";
+import { writeAsSheet } from "./spreadsheet";
 
 export class DataError extends Error {
   public readonly rowIdentifier;
@@ -28,5 +36,66 @@ export class AppError extends Error {
     super(message);
 
     this.type = type;
+  }
+}
+
+export function gatherDataErrors(
+  customers: (Customer | DataError)[],
+  contacts: (Contact | DataError)[],
+  orders: (Order | DataError)[],
+  products: (Product | DataError)[],
+  lineItems: (LineItem | DataError)[]
+) {
+  const dataErrors: DataError[] = [];
+
+  for (const item of customers) {
+    if (item instanceof DataError) dataErrors.push(item);
+  }
+  for (const item of contacts) {
+    if (item instanceof DataError) dataErrors.push(item);
+  }
+  for (const item of orders) {
+    if (item instanceof DataError) dataErrors.push(item);
+  }
+  for (const item of products) {
+    if (item instanceof DataError) dataErrors.push(item);
+  }
+  for (const item of lineItems) {
+    if (item instanceof DataError) dataErrors.push(item);
+  }
+
+  return dataErrors;
+}
+
+export function writeErrors(errors: Error[]) {
+  const rows: {
+    errorType?: string;
+    dataType?: string;
+    message?: string;
+    ["rowNumber (approx)"]?: number;
+    rowIdentifier?: string;
+  }[] = [];
+
+  for (const error of errors) {
+    if (error instanceof DataError) {
+      rows.push({
+        errorType: "Data",
+        dataType: error.type,
+        message: error.message,
+        rowIdentifier: error.rowIdentifier,
+        ["rowNumber (approx)"]: error.rowNumber,
+      });
+    } else if (error instanceof AppError) {
+      rows.push({
+        errorType: `App (${error.type})`,
+        message: error.message,
+      });
+    } else if (error instanceof Error) {
+      rows.push({
+        errorType: "Other",
+        message: error.message,
+      });
+    }
+    writeAsSheet(rows, "./output/Errors");
   }
 }
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { DataError } from "./error";
+import { AppError, DataError, gatherDataErrors, writeErrors } from "./error";
 import {
   getAllOwners,
   syncContactAsContact,
@@ -49,92 +49,158 @@ async function run() {
   const existingDeals: string[] = []; //an array of "S" numbers for all the deals that were already in HubSpot BEFORE this run of syncing
   const syncedProducts: ProductResource[] = [];
 
+  const syncErrors: Error[] = [];
+
   for (let i = 0; i < customers.length; i++) {
     const customer = customers[i];
     if (customer instanceof DataError) continue;
-    printProgress({
-      message: `Syncing customer entry ${i + 1} of ${customers.length}...`,
-      currentItem: i,
-      totalItems,
-    });
-    const { id } = await syncCustomerAsCompany(customer);
-    syncedCompanies.push({
-      hubspotId: id,
-      customerNumber: customer["Customer Number"],
-    });
+    try {
+      printProgress({
+        message: `Syncing customer entry ${i + 1} of ${customers.length}...`,
+        currentItem: i,
+        totalItems,
+      });
+      const { id } = await syncCustomerAsCompany(customer);
+      syncedCompanies.push({
+        hubspotId: id,
+        customerNumber: customer["Customer Number"],
+      });
+    } catch (error) {
+      const errorToKeep =
+        error instanceof Error
+          ? error
+          : new AppError(
+              "Unknown",
+              `Unknown error while syncing customer ${customer["Customer Name"]}`
+            );
+      syncErrors.push(errorToKeep);
+    }
   }
 
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
     if (contact instanceof DataError) continue;
-    printProgress({
-      message: `Syncing contact entry ${i + 1} of ${contacts.length}...`,
-      currentItem: i + customers.length,
-      totalItems,
-    });
-    const { id } = await syncContactAsContact(contact, syncedCompanies);
-    const { email: emailToUse } = mapContactToContact(contact);
-    syncedContacts.push({
-      hubspotId: id,
-      email: emailToUse,
-    });
+    try {
+      printProgress({
+        message: `Syncing contact entry ${i + 1} of ${contacts.length}...`,
+        currentItem: i + customers.length,
+        totalItems,
+      });
+      const { id } = await syncContactAsContact(contact, syncedCompanies);
+      const { email: emailToUse } = mapContactToContact(contact);
+      syncedContacts.push({
+        hubspotId: id,
+        email: emailToUse,
+      });
+    } catch (error) {
+      const errorToKeep =
+        error instanceof Error
+          ? error
+          : new AppError(
+              "Unknown",
+              `Unknown error while syncing contact ${contact.Name}`
+            );
+      syncErrors.push(errorToKeep);
+    }
   }
 
   for (let i = 0; i < orders.length; i++) {
     const order = orders[i];
     if (order instanceof DataError) continue;
-    printProgress({
-      message: `Syncing order entry ${i + 1} of ${orders.length}...`,
-      currentItem: i + customers.length + contacts.length,
-      totalItems,
-    });
-    const { id, syncType } = await syncOrderAsDeal(
-      order,
-      syncedCompanies,
-      syncedContacts
-    );
-    syncedDeals.push({
-      hubspotId: id,
-      salesOrderNum: order["Sales Order#"],
-    });
-    if (syncType === "update") existingDeals.push(order["Sales Order#"]);
+    try {
+      printProgress({
+        message: `Syncing order entry ${i + 1} of ${orders.length}...`,
+        currentItem: i + customers.length + contacts.length,
+        totalItems,
+      });
+      const { id, syncType } = await syncOrderAsDeal(
+        order,
+        syncedCompanies,
+        syncedContacts
+      );
+      syncedDeals.push({
+        hubspotId: id,
+        salesOrderNum: order["Sales Order#"],
+      });
+      if (syncType === "update") existingDeals.push(order["Sales Order#"]);
+    } catch (error) {
+      const errorToKeep =
+        error instanceof Error
+          ? error
+          : new AppError(
+              "Unknown",
+              `Unknown error while syncing order ${order["Sales Order#"]}`
+            );
+      syncErrors.push(errorToKeep);
+    }
   }
 
   for (let i = 0; i < products.length; i++) {
     const product = products[i];
     if (product instanceof DataError) continue;
-    printProgress({
-      message: `Syncing product entry ${i + 1} of ${products.length}...`,
-      currentItem: i + customers.length + contacts.length + orders.length,
-      totalItems,
-    });
-    const { id } = await syncProductAsProduct(product);
-    syncedProducts.push({
-      hubspotId: id,
-      sku: product.Name, //these are reversed in impress for some reason
-    });
+    try {
+      printProgress({
+        message: `Syncing product entry ${i + 1} of ${products.length}...`,
+        currentItem: i + customers.length + contacts.length + orders.length,
+        totalItems,
+      });
+      const { id } = await syncProductAsProduct(product);
+      syncedProducts.push({
+        hubspotId: id,
+        sku: product.Name, //these are reversed in impress for some reason
+      });
+    } catch (error) {
+      const errorToKeep =
+        error instanceof Error
+          ? error
+          : new AppError(
+              "Unknown",
+              `Unknown error while syncing product ${product.Name}`
+            );
+      syncErrors.push(errorToKeep);
+    }
   }
 
   for (let i = 0; i < lineItems.length; i++) {
     const lineItem = lineItems[i];
     if (lineItem instanceof DataError) continue;
-    printProgress({
-      message: `Syncing line item entry ${i + 1} of ${lineItems.length}...`,
-      currentItem:
-        i +
-        customers.length +
-        contacts.length +
-        orders.length +
-        products.length,
-      totalItems,
-    });
-    await syncLineItemAsLineItem(
-      lineItem,
-      existingDeals,
-      syncedDeals,
-      syncedProducts
-    );
+    try {
+      printProgress({
+        message: `Syncing line item entry ${i + 1} of ${lineItems.length}...`,
+        currentItem:
+          i +
+          customers.length +
+          contacts.length +
+          orders.length +
+          products.length,
+        totalItems,
+      });
+      await syncLineItemAsLineItem(
+        lineItem,
+        existingDeals,
+        syncedDeals,
+        syncedProducts
+      );
+    } catch (error) {
+      const errorToKeep =
+        error instanceof Error
+          ? error
+          : new AppError(
+              "Unknown",
+              `Unknown error while syncing line item ${lineItem["SKU#"]}`
+            );
+      syncErrors.push(errorToKeep);
+    }
   }
+
+  const allDataErrors = gatherDataErrors(
+    customers,
+    contacts,
+    orders,
+    products,
+    lineItems
+  );
+  writeErrors([...allDataErrors, ...syncErrors]);
 
   process.stdout.clearLine(0);
   console.log("Syncing complete.");
